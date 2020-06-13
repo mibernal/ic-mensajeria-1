5f8a3120f362ac0711086baf94ee6ef971b2ec13
mibernal <65865138+mibernal@users.noreply.github.com>
2020-06-13 13:44:12 -0500

    Desarrollo de UI con Firebase

---
+++ ic-mensajeria/Cliente/src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyChC29UdrLze3Kt207oPHtfgVEF-CEix6M",
+    authDomain: "ic-mensajeria.firebaseapp.com",
+    databaseURL: "https://ic-mensajeria.firebaseio.com",
+    projectId: "ic-mensajeria",
+    storageBucket: "ic-mensajeria.appspot.com",
+    messagingSenderId: "339867461549",
+    appId: "1:339867461549:web:ec255745ceb37be5c8a324",
+    measurementId: "G-JWX70XDNXJ"
+  };
